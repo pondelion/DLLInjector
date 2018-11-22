225d7f13993cb4c4c8ddcf81efe8a1d089b60a57
pondelion <programming.deve@gmail.com>
2018-11-23 02:49:27 +0900

    first committon

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace DLLInjector3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "インジェクトするDLLファイル選択";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(321, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(503, 184);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(440, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "インジェクト";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 243);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 21);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "プロセス一覧更新";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 278);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "DLL Injector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace DLLInjector3
+{
+    public partial class Form1 : Form
+    {
+
+        private string targetProcessName;
+        private System.Diagnostics.Process[] targetProcesses;
+        private IntPtr targetProcessHandle, kernel32Handle, dllpathRemoteAddr, loadLibraryAddr;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Clear();
+            updateProcessList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 「インジェクトするDLL」ボタンクリック時の処理
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "DLLファイル(*.dll)|*.dll";
+            // ファイル選択ダイアログ表示
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        // 「インジェクト」ボタンクリック時の処理
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex < 0)
+            {
+                MessageBox.Show("プロセスを選択してください");
+                return;
+            }
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("インジェクトするDLLを選択してください");
+                return;
+            }
+            targetProcessName = listBox1.SelectedItem.ToString();
+            targetProcesses = System.Diagnostics.Process.GetProcessesByName(targetProcessName);
+
+            MessageBox.Show("targetProcessName = " + targetProcessName);
+
+
+            MessageBox.Show("PID = " + targetProcesses[0].Id.ToString());
+            targetProcessHandle = OpenProcess(DesiredAccess.PROCESS_ALL_ACCESS/*DesiredAccess.PROCESS_CREATE_THREAD | DesiredAccess.PROCESS_QUERY_INFORMATION | DesiredAccess.PROCESS_VM_OPERATION | DesiredAccess.PROCESS_VM_WRITE*/, false, (uint)targetProcesses[0].Id);
+            if (targetProcessHandle == (IntPtr)null)
+            {
+                MessageBox.Show("OpenProcess() failed");
+                return;
+            }
+
+            MessageBox.Show("targetProcessHandle = " + targetProcessHandle.ToString());
+
+            String dllpath = textBox1.Text;
+            uint dllpathLength = (uint)Encoding.GetEncoding("UTF-8").GetByteCount(dllpath);
+            dllpathRemoteAddr = VirtualAllocEx(targetProcessHandle, (IntPtr)null, dllpathLength + 1, AllocationType.MEM_COMMIT, MemoryProtection.PAGE_EXECUTE_READWRITE);
+            if (dllpathRemoteAddr == (IntPtr)null)
+            {
+                MessageBox.Show("VirtualAllocEx() failed()");
+                CloseHandle(targetProcessHandle);
+                return;
+            }
+            MessageBox.Show("remoteDllpathAddr = " + dllpathRemoteAddr.ToString());
+
+            WriteProcessMemory(targetProcessHandle, dllpathRemoteAddr, dllpath, dllpathLength + 1, (IntPtr)null);
+
+            kernel32Handle = LoadLibrary("kernel32.dll");
+            loadLibraryAddr = GetProcAddress(kernel32Handle, "LoadLibraryA");
+            if (loadLibraryAddr == (IntPtr)null)
+            {
+                MessageBox.Show("GetProcAddress() failed()");
+                CloseHandle(targetProcessHandle);
+                return;
+            }
+            MessageBox.Show("loadLibraryAddr = " + loadLibraryAddr.ToString());
+
+            IntPtr retval;
+            retval = CreateRemoteThread(targetProcessHandle, (IntPtr)null, 0, loadLibraryAddr, dllpathRemoteAddr, 0, (IntPtr)null);
+            if (retval == (IntPtr)null)
+            {
+                MessageBox.Show("CreateRemoteThread() failed");
+                VirtualFreeEx(targetProcessHandle, dllpathRemoteAddr, (uint)(dllpath.Length + 1), FreeType.MEM_RELEASE);
+                CloseHandle(targetProcessHandle);
+                return;
+            }
+
+            MessageBox.Show("CreateRemoteThread retval = " + retval.ToString());
+
+            VirtualFreeEx(targetProcessHandle, dllpathRemoteAddr, (uint)(dllpath.Length + 1), FreeType.MEM_RELEASE);
+            CloseHandle(targetProcessHandle);
+        }
+
+        // 「プロセス一覧更新」ボタンクリック時の処理
+        private void button3_Click(object sender, EventArgs e)
+        {
+            updateProcessList();
+        }
+
+        // プロセス一覧をリストボックスに表示する。
+        private void updateProcessList()
+        {
+            Process[] processes = Process.GetProcesses();
+            listBox1.Items.Clear();
+            foreach (Process ps in processes)
+            {
+                listBox1.Items.Add(ps.ProcessName.ToString());
+            }
+        }
+
+        [Flags]
+        enum AllocationType
+        {
+            MEM_COMMIT = 0x1000,
+            MEM_RESERVE = 0x2000,
+        }
+
+        [Flags]
+        enum MemoryProtection
+        {
+            PAGE_EXECUTE_READWRITE = 0x40,
+        }
+
+        [Flags]
+        enum FreeType
+        {
+            MEM_RELEASE = 0x8000,
+        }
+
+        [Flags]
+        enum DesiredAccess
+        {
+            PROCESS_ALL_ACCESS = 0x1fffff,
+            PROCESS_QUERY_INFORMATION = 0x400,
+            PROCESS_CREATE_THREAD = 0x2,
+            PROCESS_VM_OPERATION = 0x8,
+            PROCESS_VM_WRITE = 0x20,
+        }
+
+        // Win32APIをインポート
+        [DllImport("Kernel32.dll")]
+        extern static IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
+
+        [DllImport("Kernel32.dll")]
+        extern static bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, FreeType dwFreeType);
+
+        [DllImport("Kernel32.dll")]
+        extern static IntPtr OpenProcess(DesiredAccess dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+
+        [DllImport("Kernel32.dll")]
+        extern static bool CloseHandle(IntPtr handle);
+
+        [DllImport("kernel32.dll")]
+        extern static bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, String lpBuffer, uint nSize, IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        extern static IntPtr LoadLibrary(String lpFileName);
+
+        [DllImport("kernel32.dll")]
+        extern static IntPtr GetProcAddress(IntPtr hModule, String lpProcName);
+
+        [DllImport("kernel32.dll")]
+        extern static IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+    }
+}
